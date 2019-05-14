bf77efb953489c2d49c8741d9093555e3bd8a0f3
shenbaga Lakshmi J <shenbaga30may@gmail.com>
2019-05-14 16:12:31 +0530

    Updated Task 3

--- src/main/java/com/stackroute/domain/task3/Main.java
+++ src/main/java/com/stackroute/domain/task3/Main.java
@@ -10,13 +10,13 @@ public class Main {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-        Movie movie = (Movie)context.getBean("movie");
-        System.out.println(movie);
+//        Movie movie = (Movie)context.getBean("movie");
+//        System.out.println(movie);
 
-//        Movie movie1 = (Movie)context.getBean("movie4");
-//        System.out.println(movie1);
+        Movie movie1 = (Movie)context.getBean("movie1");
+        System.out.println(movie1);
 
-//        Movie movie2 = (Movie)context.getBean("movie5");
-//        System.out.println(movie2);
+        Movie movie2 = (Movie)context.getBean("movie2");
+        System.out.println(movie2);
     }
 }
